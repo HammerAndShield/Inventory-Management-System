65ac61eebfbfe129ff7e2019a0d4a6518a711c8a
AustinTownsend <Austintownsend1234@gmail.com>
2023-07-15 11:46:33 -0700

    Add project files.

---
+++ C968_InventoryManagementSystem_AustinTownsend/MainScreen.cs
@@ -0,0 +1,10 @@
+namespace C968_InventoryManagementSystem_AustinTownsend
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
